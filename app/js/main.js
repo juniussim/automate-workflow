042b2a5d8f4ef1adf083df8625a60d464a748a23
Junius Sim <junius.sim@gmail.com>
2016-06-07 03:12:58 +0800

    Initial Commit

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+(function() {
+  // This function is sloppy
+}());
+
+(function() {
+  // This function is strict
+  'use strict';
+  // why do i need use strict for this document ready function
+  $(document).ready(function() {
+    console.log($);
+  });
+
+
+
+}());
+
+function add(num1, num2) {
+  return num1 + num2;
+}
---
+++ test/test.js
@@ -0,0 +1,6 @@
+describe('Add', function() {
+  it('should add two numbers together', function() {
+    expect(add(1,2)).toBe(3);
+    expect(add(3,6)).toBe(9);
+  })
+})
